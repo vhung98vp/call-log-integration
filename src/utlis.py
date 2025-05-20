efe43ccb9f0bedd19eab951e58a6ca608f5dc591
Phùng Hùng <vhung98vp@gmail.com>
2025-05-20 11:48:15 +0700

    Add handle metadata exception

--- src/utlis.py
+++ src/utlis.py
@@ -20,13 +20,11 @@ def check_relation_by_agg(log_agg):
 
 def check_relation_by_agg_metadata(log_agg, meta_A, meta_B):
     log_agg['avg_duration'] = log_agg[MES_PHONE_AGG['total_duration']] / log_agg[MES_PHONE_AGG['total_calls']]
-    meta_A['avg_duration'] = calc_avg_duration(meta_A, MES_PHONE_MD)
-    meta_B['avg_duration'] = calc_avg_duration(meta_B, MES_PHONE_MD)
-    threshold_avg_duration_A = (meta_A['avg_duration'] 
+    threshold_avg_duration_A = (calc_avg_duration(meta_A, MES_PHONE_MD) 
                                     if meta_A 
                                     and meta_A[MES_PHONE_MD['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
                                     else RL_THRESHOLDS['avg_duration'])
-    threshold_avg_duration_B = (meta_B['avg_duration'] 
+    threshold_avg_duration_B = (calc_avg_duration(meta_B, MES_PHONE_MD)
                                     if meta_B 
                                     and meta_B[MES_PHONE_MD['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
                                     else RL_THRESHOLDS['avg_duration'])
@@ -41,14 +39,12 @@ def check_relation_by_old_logs(log_agg, old_logs_agg, meta_A, meta_B):
     }    
     multiple = 1 if old_logs_agg['total_months'] <= 3 else 2
 
-    meta_A['avg_duration'] = calc_avg_duration(meta_A, ES_PHONE_PROPERTY)
-    meta_B['avg_duration'] = calc_avg_duration(meta_B, ES_PHONE_PROPERTY)
     all_logs_agg['avg_duration'] = all_logs_agg['total_duration'] / all_logs_agg['total_calls']
-    threshold_avg_duration_A = (meta_A['avg_duration'] 
+    threshold_avg_duration_A = (calc_avg_duration(meta_A, ES_PHONE_PROPERTY) 
                                     if meta_A 
                                     and meta_A[ES_PHONE_PROPERTY['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
                                     else RL_THRESHOLDS['avg_duration'])
-    threshold_avg_duration_B = (meta_B['avg_duration'] 
+    threshold_avg_duration_B = (calc_avg_duration(meta_B, ES_PHONE_PROPERTY) 
                                     if meta_B 
                                     and meta_B[ES_PHONE_PROPERTY['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
                                     else RL_THRESHOLDS['avg_duration'])
