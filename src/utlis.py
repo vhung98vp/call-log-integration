a710870dd46c8552c4a6b54791eb085a7bc71707
Hùng Phùng <hungp@Hungs-MacBook-Air.local>
2025-06-13 15:04:58 +0700

    Dev v3.0

--- src/utlis.py
+++ src/utlis.py
@@ -10,61 +10,40 @@ def build_relation_id(phone_a, phone_b, relation_type=ES_RL_CONF['relation_type'
     return str(uuid.uuid5(namespace, f"{relation_type}:{phone_a}:{phone_b}"))
 
 
-def check_relation_by_agg(log_agg):
-    if log_agg[MES_PHONE_AGG['total_duration']] >= RL_THRESHOLDS['total_duration'] or \
-      log_agg[MES_PHONE_AGG['max_duration']] >= RL_THRESHOLDS['max_duration'] or \
-      (log_agg[MES_PHONE_AGG['total_calls']] >= RL_THRESHOLDS['total_calls'] and \
-        log_agg[MES_PHONE_AGG['avg_days']] >= RL_THRESHOLDS['avg_days']):
+# CHECK RELATION
+def check_relation_by_agg(agg_list):
+    for log_agg in agg_list:
+        if log_agg[MES_PHONE_AGG['total_duration']] >= RL_THRESHOLDS['total_duration'] or \
+        log_agg[MES_PHONE_AGG['max_duration']] >= RL_THRESHOLDS['max_duration'] or \
+        (log_agg[MES_PHONE_AGG['total_calls']] >= RL_THRESHOLDS['total_calls'] and \
+            log_agg[MES_PHONE_AGG['avg_days']] >= RL_THRESHOLDS['avg_days']):
+            return True
+    return False
+
+def check_relation_by_agg_metadata(agg_list, meta_A):
+    multiple = 1 if len(agg_list) <= 3 else 2
+    agg_keys = ['total_calls', 'total_duration', 'avg_days']
+    agg_total = {k: sum(d.get(MES_PHONE_AGG[k], 0) for d in agg_list) for k in agg_keys}
+    agg_total['avg_duration'] = agg_total['total_duration'] / agg_total['total_calls'] if agg_total['total_calls'] else 0
+    threshold_avg_duration = calc_threshold_avg_duration(meta_A, MES_PHONE_MD)
+
+    if (agg_total['total_calls'] >= multiple * RL_THRESHOLDS['total_calls'] and
+            agg_total['avg_duration'] >= multiple * 0.5 * threshold_avg_duration):
         return True
     return False
 
-def check_relation_by_agg_metadata(log_agg, meta_A, meta_B):
-    log_agg['avg_duration'] = log_agg[MES_PHONE_AGG['total_duration']] / log_agg[MES_PHONE_AGG['total_calls']]
-    threshold_avg_duration_A = (calc_avg_duration(meta_A, MES_PHONE_MD) 
-                                    if meta_A 
-                                    and meta_A[MES_PHONE_MD['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
-                                    else RL_THRESHOLDS['avg_duration'])
-    threshold_avg_duration_B = (calc_avg_duration(meta_B, MES_PHONE_MD)
-                                    if meta_B 
-                                    and meta_B[MES_PHONE_MD['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
-                                    else RL_THRESHOLDS['avg_duration'])
-    if log_agg['avg_duration'] >= 0.5 * min(threshold_avg_duration_A, threshold_avg_duration_B):
-        return True
-    return False
 
-def check_relation_by_old_logs(log_agg, old_logs_agg, meta_A, meta_B):
-    all_logs_agg = {
-        'total_calls': old_logs_agg['total_calls'] + log_agg[MES_PHONE_AGG['total_calls']],
-        'total_duration': old_logs_agg['total_duration'] + log_agg[MES_PHONE_AGG['total_duration']],
-    }    
-    multiple = 1 if old_logs_agg['total_months'] <= 3 else 2
+# ADDITIONAL UTILS
 
-    all_logs_agg['avg_duration'] = all_logs_agg['total_duration'] / all_logs_agg['total_calls']
-    threshold_avg_duration_A = (calc_avg_duration(meta_A, ES_PHONE_PROPERTY) 
-                                    if meta_A 
-                                    and meta_A[ES_PHONE_PROPERTY['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
-                                    else RL_THRESHOLDS['avg_duration'])
-    threshold_avg_duration_B = (calc_avg_duration(meta_B, ES_PHONE_PROPERTY) 
-                                    if meta_B 
-                                    and meta_B[ES_PHONE_PROPERTY['total_calls']] > 2 * RL_THRESHOLDS['total_calls'] 
-                                    else RL_THRESHOLDS['avg_duration'])
-
-    if all_logs_agg['total_duration'] >= multiple * RL_THRESHOLDS['total_duration'] and \
-      all_logs_agg['total_calls'] >= multiple * RL_THRESHOLDS['total_calls'] and \
-      all_logs_agg['avg_duration'] >= 0.5 * min(threshold_avg_duration_A, threshold_avg_duration_B):
-        return True
-    
-    return False
-
-
-def calc_avg_duration(metadata, property):
-    if metadata and metadata[property['total_calls']] > 0:
-        total_calls_from = metadata[property['total_calls']] * metadata[property['call_from_rate']]
-        total_calls_to = metadata[property['total_calls']] - total_calls_from
-        total_duration_from = metadata[property['avg_duration_from']] * total_calls_from
-        total_duration_to = metadata[property['avg_duration_to']] * total_calls_to
-        return (total_duration_from + total_duration_to) / metadata[property['total_calls']]
-    return 0
+def calc_threshold_avg_duration(meta_list, property):
+    total_calls_all, total_duration_all = 0, 0
+    for meta in meta_list:
+        total_calls_all += meta[property['total_calls']]
+        total_duration_from = meta[property['avg_duration_from']] * meta[property['total_calls']] * meta[property['call_from_rate']]
+        total_duration_to = meta[property['avg_duration_to']] * meta[property['total_calls']] * (1 - meta[property['call_from_rate']])
+        total_duration_all += total_duration_from + total_duration_to
+    avg_duration_all = total_duration_all / total_calls_all if total_calls_all else 0
+    return avg_duration_all if total_calls_all > 2 * RL_THRESHOLDS['total_calls'] else RL_THRESHOLDS['avg_duration']
 
 
 def build_output_message(phone_a, phone_b):
